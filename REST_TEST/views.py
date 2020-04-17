43b8ae734e00e0d628236c2c2787c1b1cea6cc80
Виктор <scren2010@gmail.com>
2020-04-17 19:40:23 +0600

    add PUT PATCH aтв DELETE

--- REST_TEST/views.py
+++ REST_TEST/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework import request, status
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework.views import APIView
+
+from .serializers import HelloSerializer
 
 
 class HellowWORLD(APIView):
+
+    serializer_class = HelloSerializer
+
     def get(self, request, format=None):
        
         an_apiview = [
@@ -13,3 +18,24 @@ class HellowWORLD(APIView):
             'dasda211d121fasfasdfafdasf',
         ]
         return Response({'messege': 'Hello!', 'an_apiview': an_apiview})
+    def post(self, request):
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            messege = "Привет сукчка {0}".format(name)
+
+            return Response({"message": messege})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk=None):
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
