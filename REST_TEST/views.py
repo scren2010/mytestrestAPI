0aeddfbfedb954cdcac4cc1e5bb8143daab17fe6
Виктор <scren2010@gmail.com>
2020-04-17 18:35:42 +0600

    APIview.

--- REST_TEST/views.py
+++ REST_TEST/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+class HellowWORLD(APIView):
+    def get(self, request, format=None):
+       
+        an_apiview = [
+            'dsdsdasdasdasdasd',
+            'dasdasdasdasdasd23343',
+            'dasda211d121fasfasdfafdasf',
+        ]
+        return Response({'messege': 'Hello!', 'an_apiview': an_apiview})
