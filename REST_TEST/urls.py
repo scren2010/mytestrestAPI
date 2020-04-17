cc5d59db32e1e99c62bcb02e19e7cccfb1e42c99
Виктор <scren2010@gmail.com>
2020-04-17 20:10:05 +0600

    ViewSet and Router

--- REST_TEST/urls.py
+++ REST_TEST/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path 
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("vik", views.HelloWorldSETS, basename='viktor')
 
 urlpatterns = [
     path('hello/', views.HellowWORLD.as_view()),
+    path('', include(router.urls)),
 ]
 
